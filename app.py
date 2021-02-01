9341947d2840ebaa6f68c86623ae98ee38c69c8c
Simbamon <37096378+Simbamon@users.noreply.github.com>
2021-02-01 20:19:34 +0900

    Third commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from forms import PredictionForm
+from flask import json
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Secretkeys'
@@ -16,13 +18,23 @@ def index():
 def predict():
     form = PredictionForm()
     if form.validate_on_submit():
-        python_object = [form.month.data, form.dayofweek.data]
-        userInput = []
-        userInput.append(python_object)
-        payload_scoring = {"input_data": [{"fields": ["month", "dayofweek", "d"], 
-        "values": userInput }]}
-        output = payload_scoring
+        objects = [form.month.data, form.dayofweek.data, form.borough.data, form.min_humidity.data, form.max_humidity.data,
+                         form.min_temp.data, form.max_temp.data, form.max_wind_speed.data, form.weather_description.data
+        ]
 
+        userInput = []
+        userInput.append(objects)
+        payload_scoring = {"input_data": [{"fields": ["month", "dayofweek", "borough", "min_humidity", "max_humidity", "min_temp",
+                           "max_temp", "max_wind_speed", "weather_description"], 
+                           "values": userInput }]}
+        
+        response_scoring = requests.post("", json=payload_scoring, headers=header)
+
+        output = json.loads(response_scoring.text)
+
+        
+
+        
         return output
     return render_template('index.html', form = form)
 
